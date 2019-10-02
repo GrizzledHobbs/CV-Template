3d4f13028925fd69b049082b866ee8370c2fd16c
Cody Hobbs <chobbs31@gmail.com>
2019-10-02 00:43:06 -0400

    Add files via upload

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,8 @@
 function myFunction() {
-    var x = document.getElementById("myNavbar");
-    if (x.className === "navbar") {
+    var x = document.getElementById("myNav");
+    if (x.className === "nav") {
         x.className += " responsive";
     } else {
-        x.className = "navbar";
+        x.className = "nav";
     }
-} 
+}
